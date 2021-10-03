fbbd461e54bfd38604e3aad6aa02e025dfb56c02
Ishaan-11 <ishaansharma1998@gmail.com>
2021-10-03 20:39:32 +0530

    add: delete task feature

--- src/App.js
+++ src/App.js
@@ -22,10 +22,22 @@ function App() {
     },
   ]);
 
+  function deleteTask(id) {
+    setTasks(tasks.filter((task) => task.id !== id));
+  }
+
   return (
     <div className="container">
       <Header />
-      <Tasks tasks={tasks}/>
+      {
+        tasks.length > 0 ? 
+        <Tasks 
+          tasks={tasks}
+          onDelete={deleteTask}
+        />
+        :
+        'No Tasks to show'
+      }
     </div>
   );
 }
